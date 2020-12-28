9ab7d261ccb80d2e0f5bf6b3ee391c4ca4b6e399
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-28 15:19:33 -0300

    fix:show utilitariio name in json

--- Controllers/ProdutoesController.cs
+++ Controllers/ProdutoesController.cs
@@ -24,7 +24,7 @@ namespace Armazen_API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Produto>>> Getprodutos()
         {
-            return await _context.produtos
+            return await _context.produtos.Include("utilitario")//equals not ignore case: search for json name
                 .ToListAsync();
         }
 
@@ -33,8 +33,8 @@ namespace Armazen_API.Controllers
         public async Task<ActionResult<Produto>> GetProduto(int id)
         {
             var produto = await _context.produtos
-                
-                .FindAsync(id);
+                .Include("utilitario").FirstOrDefaultAsync(prd=>prd.id==id);
+                //.FindAsync(id);
 
             if (produto == null)
             {
